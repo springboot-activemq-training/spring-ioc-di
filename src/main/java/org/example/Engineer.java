588d5f556b67063aba0518fa67b9fbbd679409c4
Frederick Frimpong Arthur <frederickarthur30@gmail.com>
2023-06-05 10:23:42 +0000

    Added engineer class

---
+++ src/main/java/org/example/Engineer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Engineer {
+
+    public void designAlgorith(){
+        System.out.println("Engineer is designing algorithm");
+    }
+    public void developAlgorithm(){
+        System.out.println("Engineer is developing algorithm");
+    }
+}
